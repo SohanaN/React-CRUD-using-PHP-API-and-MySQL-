3fcd1a5b27859e9c09faadb0de5ecc970e5d9073
SohanaN <sohananaz01@gmail.com>
2023-03-27 00:09:52 +0600

    show data in list page

--- src/components/ListUser.js
+++ src/components/ListUser.js
@@ -1,9 +1,47 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const ListUser = () => {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        getUsers();
+    }, []);
+    function getUsers() {
+        axios.get('http://localhost/react-with-php-mysql-server/users/').then(function(response) {
+            console.log(response.data);
+            setUsers(response.data);
+        });
+    }
     return (
         <div>
-              <h1>List Users</h1>
+            <h1>List Users</h1>
+            <table>
+                <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>Name</th>
+                        <th>Email</th>
+                        <th>Mobile</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user, key) =>
+                        <tr key={key}>
+                            <td>{user.id}</td>
+                            <td>{user.name}</td>
+                            <td>{user.email}</td>
+                            <td>{user.mobile}</td>
+                            <td>
+                                <Link to={`user/${user.id}/edit`} style={{marginRight: "10px"}}>Edit</Link>
+                                <button>Delete</button>
+                            </td>
+                        </tr>
+                    )}
+
+                </tbody>
+            </table>
         </div>
     );
 };
